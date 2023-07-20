4c6696496fc1c0651baee28246402701de93ee1b
Audrey Pachol <audrey.pachol@gmail.com>
2023-07-20 12:42:37 +0200

    update
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,6 +4,7 @@ class Car < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
+  has_one_attached :photo
   has_many :bookings, dependent: :destroy
   validates :brand, presence: true
   validates :model, presence: true
